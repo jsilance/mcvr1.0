5cdf60509446bf47b96852c6e1f6497e3b41236d
jsilance <52762222+jsilance@users.noreply.github.com>
2020-09-02 17:29:04 +0200

    Add files via upload

--- ads_config.sh
+++ ads_config.sh
@@ -9,9 +9,10 @@ sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 -y
 sudo apt-get install libatlas-base-dev -y
 sudo apt-get install libjasper-dev -y
 
+sudo apt install python3-pip
 sudo pip3 install pillow --upgrade
 
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 
-sudo pip install opencv-contrib-python==4.1.0.25
+sudo pip install opencv-contrib-python==4.4.0.42
